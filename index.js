b6db5c17ef7002fd38fa156f1e56f772f361f74c
Larry Motuzis <larrymotuzis@gmail.com>
2022-01-24 15:39:15 -0600

    updated index.js

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 fetch('https://api.thedogapi.com/v1/breeds')
     .then(res => res.json())
-    .then(dogImages => iterateDogs(dogImages))
+    .then(dogImages => grabFiveDogs(dogImages))
 
-function iterateDogs(dogImages) {
-    dogImages.forEach(singleBreed => {
-        renderDogImages(singleBreed.image.url)
+    function grabFiveDogs(dogImages) {
+        const fiveDogImages = dogImages.slice(0,5)
+            console.log(fiveDogImages)
+            iterateDogs(fiveDogImages)
+            
+    }
 
+    function iterateDogs(fiveDogImages) {
+        fiveDogImages.forEach(singleBreed => {
+           renderDogImages(singleBreed.image.url)
         })
     }
 
+
+
 function renderDogImages(breedURL) {
-    let body = document.querySelector('body');
-    let newImg = document.createElement('img')
+    let newImg = document.createElement('img');
+    let header = document.querySelector('header');
+    header.insertAdjacentElement('afterend', newImg)
     newImg.src = breedURL
-    body.appendChild(newImg)
     
-}
+    
+};
